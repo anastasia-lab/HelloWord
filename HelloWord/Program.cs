3233cfeac250cf8cc60ae033d520f8dee720a652
anastasia-lab <anastasiya.shpilevaia@ictis.sfedu.ru>
2022-03-15 21:52:50 +0300

    Добавьте файлы проекта.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задание 1
+
+            Console.WriteLine("Hello World !!!");
+            Console.ReadKey();
+
+            #endregion
+
+            #region Задание 2
+
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+            Console.ReadLine();
+
+            #endregion
+        }
+    }
+}
